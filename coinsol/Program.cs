0f0aefcc7c57f24adb5c1cb7a75cf9d29e10ceb4
tella <n00477869@upn.pe>
2025-09-05 09:52:46 -0500

    subiendo archivo principal

---
+++ coinsol/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinsol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ejer1();
+            Console.ReadKey();//relizando detenemiento de consola 
+        }
+        static void ejer1()
+
+        {
+            string nombre, carrera;
+
+            Console.Write("INGRESE SU NOMBRE:");
+            nombre = Console.ReadLine();
+
+            Console.Write("ingrese su carrera:");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre},bienvenido al curso FA DE {carrera}");
+        }
+    }
+
+}
---
+++ semana  1 py/semana__1_py.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre=input("ingrese su nombre:")
+    carrera =input ("Ingrese su carreera:")
+
+    print(f"\n{nombre},bienvenido al curso FA DE {carrera}")
+
+ejer1()
